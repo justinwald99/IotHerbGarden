cf94a645c44a3710c2f0aac7c3897d4711049575
Parker <plkillia@ncsu.edu>
2021-04-11 17:09:45 -0400

    Added sampling logic to garden_monitor, missing MQTT

--- garden_monitor.py
+++ garden_monitor.py
@@ -1,16 +1,18 @@
 import sys
 import time
 import json
+import datetime
 
 import paho.mqtt.client as mqtt
 from ADC_Library import ADS7830
 from gpiozero import GPIODevice
 import dht11
+import asyncio
 
 adc = ADS7830()
 # soil sensors channel: 0, 1, 2, 3
 # light sensor channel: 4
-ambientySensor = dht11.DHT11(pin = 11)
+ambientSensor = dht11.DHT11(pin = 11)
 # result = ambientSensor.read
 # temp = result.temperature
 # humidity = result.humidity
@@ -26,14 +28,9 @@ pump_mapping = {
     "pump4":pump4
 }
 
-sensors = []
-
-#       (id: <int>):
-        #(type: <string>,
-        #name: <string>,
-        #unit: <string>,
-        #sample_rate_hz: <int>)
-
+sensors = {} ## TODO: Hard code default values
+# ids 0, 1, 2, 3 will be soil sensors. id=4 will be light,
+# id=5 will be temperature from DHT, id=6 will be ambient humidity from DHT
 
 def on_connect(mqttc, obj, flags, rc):
     print("rc: " + str(rc))
@@ -51,23 +48,53 @@ def on_control_message(client, userdata, msg):
         
 
 def on_sensor_info(client, userdata, msg):
+    global sensors
     sensor_info = json.loads(msg)
-    # collect all known sensor ids
-    known_ids = []
-    for sensor in sensors:
-        known_ids.append(sensor["id"])
+    #For sensors that are passed, we change our local config
+    knownids = []
     for sensor in sensor_info:
-        if sensor["id"] in known_ids:
-            for known_sensor in sensors:
-                if sensor["id"] == known_sensor["id"]:
-                    sensors[sensors.index(known_sensor)] = sensor
-                    known_sensor = sensor
-                    break
-            
+        if sensor["id"] in dict.keys(sensors):
+            sensors[sensor["id"]] = {
+                "type":sensor["type"],
+                "name":sensor["name"],
+                "unit":sensor["unit"], 
+                "sample_rate_hz":sensor["sample_rate_hz"]
+            }
+            knownids.append("id")
+    #For sensors that sensor info didn't know about, we pass them through sensors/config/id
+    # monitor - knownids = unknown to manager
+    for index, sensor in dict.items(sensors):
+        if index not in knownids:
+            mqttc.publish(f"sensors/config/{index}",
+                 payload=json.dumps({"id":index,
+                    "type":sensor["type"],
+                    "name":sensor["name"],
+                    "unit":sensor["unit"],
+                    "sample_rate_hz":sensor["sample_rate_hz"]}), qos=2, retain=False)
 
 
+def sample(sensor):
+    print("wip")
+    if sensor.key in range(0,5):
+        adc.analogRead(sensor.key)
+        # TODO: send MQTT message with results
+    elif sensor.key == 5: #read temperature
+        pass # TODO: Figure out what library and methods we need to use to read from DHT
+        # TODO: send MQTT message with results
+    elif sensor.key == 6: #read humidity
+        pass # TODO: Figure out what library and methods we need to use to read from DHT
+        # TODO: send MQTT message with results
 
-def sample_routine():
+
+def sample_routine(): ## TODO: code the sample routine
+    # guaranteed to have received the retained sensors info if it exists, don't have to 
+    # poll again until the end of the loop.
+    last_sampled = { sensor_id:datetime.datetime.now() for sensor_id in sensors.keys() }
+    while(1):
+        for id, sample_timestamp in last_sampled.items:
+            if (datetime.datetime.now() - sample_timestamp).total_seconds() > (sensors[id]["samples"]):
+                sample(sensors[id])
+                last_sampled[id] = datetime.datetime.now()
     
 
 # on startup, sensor info is empty
@@ -90,7 +117,7 @@ if __name__ == '__main__':
     # will be subscribed to sensors/info, which is a list of all sensor dictionaries
     mqttc.connect(sys.argv[1], 1883)
 
-    sample_routine()
+    asyncio.run(sample_routine())
     # store retained messages from config - check
     # use that config to sample
     # after sampling, send to sensors/data/{id}
