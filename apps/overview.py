ecd767e717fd8f56d4beaaf7e75ce613d4fdf278
Parker <plkillia@ncsu.edu>
2021-04-19 18:53:20 -0400

    WIP: Fixed SQL Queries in overview.py

--- apps/overview.py
+++ apps/overview.py
@@ -2,11 +2,13 @@ import dash_html_components as html
 import dash_core_components as dcc
 from utils.ui_elements import make_gauge
 from dash.dependencies import Input, Output, State
-from sqlalchemy import create_engine, text
 from app import app
+
+from sqlalchemy import create_engine, text, Table, desc, select
+from sqlalchemy.sql.schema import MetaData
+
 import plotly.express as px
 import pandas as pd
-import plotly.graph_objects as go
 
 
 @app.callback(
@@ -16,17 +18,31 @@ import plotly.graph_objects as go
     Input('update_page', 'n_intervals')
 )
 def update_page(n_intervals):
-    conn = create_engine("sqlite+pysqlite:///garden.db", future=False).connect()
-    try:
-        humidity_result = conn.execute(text("SELECT TOP 1 value FROM sample INNER JOIN sensor ON sample.sensor_id = sensor.id WHERE type = N'ambient_humidity' ORDER BY sample.id DESC"))
-        temp_result = conn.execute(text("SELECT TOP 1 value FROM sample INNER JOIN sensor ON sample.sensor_id = sensor.id WHERE type = N'ambient_temperature' ORDER BY sample.id DESC"))
-        light_result= conn.execute(text("SELECT TOP 100 timestamp,value FROM sample INNER JOIN sensor ON sample.sensor_id = dbo.sensor.id WHERE type = N'light' ORDER BY sample.id DESC"))
-    except:
-        print("Error occurred collecting DB data, passing test values")
-        return [make_gauge("Temperature", 50, "° F", [0, 100], "#dc3545"), make_gauge("Relative Humidity", 50, "%", [0, 100], "#17a2b8"), px.line(pd.DataFrame({'timestamp':[1,2,3,4],'sunlight':[2,3,4,5]}), x='timestamp', y='sunlight', title='Test Input', width=525, height=250)]#  go.Figure(data=go.Scatter(x=[1,2,3,10],y=[1,2,3,4], mode="lines"), layout=go.Layout(title="Invalid", height=150, width=400)) # Fix overall spacing, title spacing, add labels to axes
+    engine = create_engine("sqlite+pysqlite:///garden.db", future=True)
+    metadata = MetaData()
+    test = 0
+    humidity_result, temp_result, light_result = (None, None, None)
+    with engine.connect() as conn:
+        try:
+            sample_table = Table("sample", metadata, autoload_with=engine)
+            sensor_table = Table("sensor", metadata, autoload_with=engine)
+            humidity_result = conn.execute(
+                select(sample_table.c.value).where(sample_table.c.sensor_id == sensor_table.c.id, sensor_table.c.type == 'ambient_humidity').limit(1).order_by(desc(sample_table.c.timestamp))
+            ).fetchall()
+            temp_result = conn.execute(
+                select(sample_table.c.value).where(sample_table.c.sensor_id == sensor_table.c.id, sensor_table.c.type == 'ambient_temperature').limit(1).order_by(desc(sample_table.c.timestamp))
+            ).fetchall()
+            light_result= conn.execute(
+                select(sample_table.c.value).where(sample_table.c.sensor_id == sensor_table.c.id, sensor_table.c.type == 'light').limit(100).order_by(desc(sample_table.c.timestamp))
+            ).fetchall()
+        except:
+            print("Error occurred collecting DB data, passing test values")
+            test = 1
+    if None in (humidity_result, temp_result, light_result) or test == 1:
+        return [make_gauge("Temperature", 50, "° F", [0, 100], "#dc3545"), make_gauge("Relative Humidity", 50, "%", [0, 100], "#17a2b8"), px.line(pd.DataFrame({'timestamp':[1,2,3,4],'sunlight':[2,3,4,5]}), x='timestamp', y='sunlight', title='Test Input', width=525, height=250)]
+    df = pd.DataFrame(light_result, columns=["timestamp","value"])
     return [make_gauge("Temperature", temp_result[0]["value"], "° F", [0, 100], "#dc3545"), make_gauge("Relative Humidity", humidity_result[0]["value"], "%", [0, 100], "#17a2b8"), 
-    go.Figure(data=go.Scatter(x=light_result.keys,y=light_result.values, mode="lines"), layout=go.Layout(title="Sunlight", height=250, width=500))]
-    #px.line(pd.DataFrame.from_dict(light_result, orient="index"), x="timestamp", y="value", title='Life expectancy in Canada')
+        px.line(df, x='timestamp', y=df.value, title='Sunlight', width=525, height=250)]
     # only using the last result from the query
 
 layout = [
